28c482574ab30575027c7908ef8f03626f4999fd
Noccarc <67786566+Noccarc@users.noreply.github.com>
2022-10-05 13:31:30 +0530

    Update knob.sh
    Added setup parameters.

--- recipes-core/systemd/systemd-knob/knob.sh
+++ recipes-core/systemd/systemd-knob/knob.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+echo "quadrature x4" > /sys/bus/counter/devices/counter0/count0/function
+echo 65535 > /sys/bus/counter/devices/counter0/count0/ceiling
+echo 0 > /sys/bus/counter/devices/counter0/count0/count
+echo 1 > /sys/bus/counter/devices/counter0/count0/enable
 exec /usr/local/scripts/knob_730i.sh
